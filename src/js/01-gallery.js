94c5a74b1d3e295c6a811b4207c90d13c53ea8c1
Volovodenko Taras <taras.volovodenko@gmail.com>
2023-01-22 17:13:18 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,4 +5,20 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+const markup = galleryItems
+    .map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt=${description}/></a>`;
+    })
+    .join("");
+
+galleryRef.innerHTML = markup;
+
+new SimpleLightbox(".gallery a", {
+    captionsData: 'alt',
+    loop: true,
+    captionDelay: 250,
+    animationSpeed: 300,
+    animationSlide: true,
+});
